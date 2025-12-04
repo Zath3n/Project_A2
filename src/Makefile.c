e4b4d050f0701e3a7858b38e97f327cb7c0e86c0
Zath3n <zathen101@gmail.com>
2025-12-03 20:11:48 -0700

    s
    s

--- src/Makefile.c
+++ src/Makefile.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
-#include <curses.h>
-#include <unistd.h>
+#ID:3132175, Name:Gabriel
 
+CC = gcc
+CFLAGS = -Wall -std=c11
+LDFLAGS = -lm
 
+appGame: simpleMazeGame.c
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
 
+%.o: %.c %.h
+	$(CC) $(CFLAGS) -c $<
+
+.PHONY:
+
+clean:
+	rm -f *.o appTestPart2.out
+
+run: appGame
+	./appGame
