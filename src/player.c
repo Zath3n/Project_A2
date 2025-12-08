b5e33d5477d51606ba3154c18eb4ded24669d1ba
Zath3n <zathen101@gmail.com>
2025-12-07 22:05:00 -0700

    Doxygen Update
    Check

--- src/player.c
+++ src/player.c
@@ -1,26 +1,29 @@
 #include <ncurses.h>
 #include "player.h"
 
+
 void init_player(Player *p) {
     p->x = 2;
     p->y = 2;
     p->symbol = '@';
 }
 
+
 void draw_player(Player *p) {
     mvaddch(p->y, p->x, p->symbol);
 }
+
 
 void player_move(Player *p, Level *lvl, int key) { //Player movement
     int nx = p->x;
     int ny = p->y;
 
-    if (key == 'w' || key == 'W') ny--;
-    if (key == 's' || key == 'S') ny++;
-    if (key == 'a' || key == 'A') nx--;
-    if (key == 'd' || key == 'D') nx++;
+    if (key == 'w' || key == 'W') ny--; //moves player up (Y-axis)
+    if (key == 's' || key == 'S') ny++; //moves player down (Y-axis)
+    if (key == 'a' || key == 'A') nx--; //moves player left (X-axis)
+    if (key == 'd' || key == 'D') nx++; //moves player right (X-axis)
 
-    if (is_walkable(lvl, nx, ny)) {
+    if (is_walkable(lvl, nx, ny)) { //Checks if the new postion/coordinate is an empty spot and not a border/wall.
         p->x = nx;
         p->y = ny;
     }
--- src/player.h
+++ src/player.h
@@ -3,14 +3,49 @@
 
 #include "levels.h"
 
+
+/**
+ * 
+ * Stores the users current coordinates/position and display symbol.
+ * 
+ */
 typedef struct {
     int x, y;
     char symbol;
 } Player;
 
+
+/**
+ * 
+ * Initializes the player with startng values by
+ * setting the player's position based on x/y value and symbol represented by @.
+ * 
+ */
 void init_player(Player *p);
+
+
+/**
+ * 
+ * Draws the player on the Ncurses screen, using mvaddch() to place @, 
+ * at the given location/position
+ * 
+ */
 void draw_player(Player *p);
+
+/**
+ * 
+ * Moves the user based on user keyboard input.
+ * Allows the user to move the character with WASD key. If one said button is pressed, 
+ * the current location of @ in the console is moved in the direction of the key pressed.
+ * 
+ */
 void player_move(Player *p, Level *lvl, int key);
+
+/**
+ * 
+ * Checks if the player symbol has reached the exit of the level, which is represented by "F".
+ * 
+ */
 int player_reached_exit(Player *p, Level *lvl);
 
 #endif
